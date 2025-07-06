2228de544deba9ce77d2b13bd5e2e801adcef4f1
Cristian Prince <62496847+CrisKop@users.noreply.github.com>
2025-07-06 16:43:41 -0500

    Create template from a existing scrim in db

--- src/commands/slash/CreateScrimExcelTemplate.ts
+++ src/commands/slash/CreateScrimExcelTemplate.ts
@@ -2,39 +2,92 @@ import type BaseClient from "#lib/BaseClient.js";
 import Command from "#lib/structures/Command.js";
 import { generarPlantillaScrimExcel } from "#lib/utils/Tablas/GenerateExcelTemplate.js";
 import type { ChatInputCommandInteraction } from "discord.js";
+import type { AutocompleteInteraction } from "discord.js";
+import { Database } from "#lib/Database.js";
+import { Db as Configuration } from "#lib/Configuration.js";
+import type { Collection } from "mongodb";
+import { ObjectId } from "mongodb";
+
+const db = await Database.getInstance(Configuration).connect();
 
 export default class extends Command {
+  protected scrimCollection: Collection<Scrim>;
+
   public constructor(client: BaseClient) {
     super(client, {
       name: "create_scrim_excel_template",
       description: "Create a template for scrim matches in Excel format.",
       memberPermissions: ["Administrator"],
     });
+
+    this.scrimCollection = db.collection<Scrim>("scrims");
   }
 
   public async execute(
     interaction: ChatInputCommandInteraction<"cached" | "raw">
   ) {
-    const matchesCount = interaction.options.getString("matches_count", true);
-    const playersPerTeam = interaction.options.getString(
-      "players_per_team",
-      true
-    );
-    const teamsQuantity = interaction.options.getString("teams", true);
+    const scrim = interaction.options.getString("scrim", true);
+
+    if (!scrim) {
+      return interaction.reply({
+        content: "❌ You must select a scrim.",
+        ephemeral: true,
+      });
+    }
+
+    // Check if the scrim exists in the database
+    console.log(`Scrim selected: ${scrim}`);
+
+    if (!ObjectId.isValid(scrim)) {
+      throw new Error("ID inválido");
+    }
+
+    const scrimData = await this.scrimCollection.findOne({
+      _id: new ObjectId(scrim),
+    });
+
+    if (!scrimData) {
+      return interaction.reply({
+        content: "❌ The selected scrim does not exist.",
+        ephemeral: true,
+      });
+    }
+
+    const matchesCount = 3; // Default value, can be changed later
+    const playersPerTeam = 4; // Default value, can be changed later
+    const teamsQuantity = scrimData.maxTeams;
 
     const file = await generarPlantillaScrimExcel({
-      numPartidas: parseInt(matchesCount, 10),
-      jugadoresPorEquipo: parseInt(playersPerTeam, 10),
-      equipos: parseInt(teamsQuantity, 10),
+      numPartidas: matchesCount,
+      jugadoresPorEquipo: playersPerTeam,
+      equipos: teamsQuantity,
     });
 
     await interaction.reply({
       content:
         "✅ Here is your template for the scrim:" +
-        +`\n\n📊 Matches: ${matchesCount}` +
+        `\n\n📊 Matches: ${matchesCount}` +
         `\n👥 Players per team: ${playersPerTeam}` +
         `\n🛡️ Teams: ${teamsQuantity}`,
       files: [{ attachment: file, name: "template.xlsx" }],
     });
+  }
+
+  override async autocomplete(interaction: AutocompleteInteraction) {
+    const focusedOption = interaction.options.getFocused(true);
+
+    if (focusedOption.name !== "scrim") return;
+
+    const scrims = await this.scrimCollection
+      .find({ name: { $regex: focusedOption.value, $options: "i" } })
+      .limit(25)
+      .toArray();
+
+    const choices = scrims.map((scrim) => ({
+      name: scrim.name.slice(0, 100), // texto visible
+      value: scrim._id.toString().slice(0, 100), // valor enviado al bot
+    }));
+
+    await interaction.respond(choices);
   }
 }
