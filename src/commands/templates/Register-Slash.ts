d9f10f2ce31975a88b4e3238ecb1ffd6f8e0b8c0
A <ssjlonely730@gmail.com>
2025-07-06 04:03:26 +0000

    docs: Templates

---
+++ src/commands/templates/Register-Slash.ts
@@ -0,0 +1,25 @@
+import { type APIApplicationCommand, ApplicationCommandType } from 'discord-api-types/v10';
+
+export default {
+	name: 'example-command',
+	description: 'example-command',
+	type: ApplicationCommandType.ChatInput,
+	dm_permission: true // esto cambialo depende mucho si quieres que el comando se ejecute en dm o no
+} as APIApplicationCommand; /*
+    options: [
+        {
+            name: 'name', Esto ya es los parametros que le pases al comando ej, equipos, etc...
+            description: '',
+            type: ApplicationCommandOptionType.User || ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: 'admin',
+            description: '',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+            autocomplete: true
+        }
+    ] as APIApplicationCommandOption[],
+    dm_permission: false
+*/
